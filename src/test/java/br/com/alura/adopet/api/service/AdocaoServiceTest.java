d3667d261988c2a7902cd9057af4fcfab6b73fde
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-08-12 23:34:56 -0300

    TEST: Implementando a classe de test de adocaoService do método Solicitar.

---
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -0,0 +1,66 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDTO;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class AdocaoServiceTest {
+
+    @InjectMocks //classe principal, que queremos testar, para o mockito injetar todos os mocks lá dentro.
+    private AdocaoService adocaoService;
+
+    @Mock
+    private AdocaoRepository adocaoRepository;
+
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private TutorRepository tutorRepository;
+
+    @Mock
+    private EmailService emailService;
+
+    @Mock
+    private List<ValidacaoSolicitacaoAdocao> validacoes;
+
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private Tutor tutor;
+
+    @Mock
+    private Abrigo abrigo;
+
+    @Mock
+    private SolicitacaoAdocaoDTO dto;
+
+    @Test
+    void deveriaSalvarAdocaoAoSolicitar() {
+        //Arrange
+
+        //ACT
+        adocaoService.solicitar(dto);
+
+        //ASSERT
+        then(petRepository).should().save(any()); //verifica se o petRepository teve o método save chamado com qualquer parâmetro
+
+    }
+}
