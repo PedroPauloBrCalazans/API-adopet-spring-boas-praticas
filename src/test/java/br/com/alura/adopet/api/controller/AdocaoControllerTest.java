0a15128d7043520e87577284ee85f7b8d41d987a
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-08-13 23:55:33 -0300

    TEST: Corpo do método para testar a controller de adoção.

---
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -0,0 +1,19 @@
+package br.com.alura.adopet.api.controller;
+
+import org.junit.jupiter.api.Test;
+
+class AdocaoControllerTest {
+
+    @Test
+    void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() {
+        //Arrange - dados que tenho de informações, para testar a classe
+
+
+        //ACT
+
+
+        //ASSERT -
+
+    }
+
+}
