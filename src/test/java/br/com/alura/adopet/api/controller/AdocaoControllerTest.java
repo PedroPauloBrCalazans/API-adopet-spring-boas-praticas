3e3e111541385bfd94ebc914f7be9e3e0196e5dc
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-08-14 00:29:43 -0300

    TEST: Implementação do método simulando uma requisição post com retorno 200.

--- src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -1,14 +1,14 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.service.AdocaoService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import javax.print.attribute.standard.Media;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -18,6 +18,9 @@ class AdocaoControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @MockBean
+    private AdocaoService service;
 
     @Test
     void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() throws Exception {
@@ -35,6 +38,31 @@ class AdocaoControllerTest {
 
         //ASSERT
         Assertions.assertEquals(400, response.getStatus());
+
+    }
+
+    @Test
+    void deveriaDevolverCodigo200ParaSolicitacaoDeAdocaoSemErros() throws Exception {
+        //Arrange
+        String json = """
+                    {
+                        "idPet": 1,
+                        "idTutor": 1,
+                        "motivo": "Motivo qualquer"
+                    }
+                """;
+
+
+        //ACT
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+
+        //ASSERT
+        Assertions.assertEquals(200, response.getStatus());
 
     }
 }
