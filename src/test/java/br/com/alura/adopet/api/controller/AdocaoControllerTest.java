1cacd5dd55c973b42bf84e6cd2f5329f5430770b
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-08-14 00:19:56 -0300

    TEST: Implementação do método simulando uma requisição post.

--- src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -1,19 +1,43 @@
 package br.com.alura.adopet.api.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import javax.print.attribute.standard.Media;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class AdocaoControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() {
-        //Arrange - dados que tenho de informações, para testar a classe
+    void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() throws Exception {
+        //Arrange
+        String json = "{}";
 
 
         //ACT
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
 
 
-        //ASSERT -
+        //ASSERT
+        Assertions.assertEquals(400, response.getStatus());
 
     }
-
 }
+
+//simular uma requisição o spring tem algumas classes, que auxiliam esses tipos de testes
+//MockMvc -> classe que simulam requisições
